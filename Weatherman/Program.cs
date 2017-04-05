c445f6b58137e9bb2f8cfd5de5c8bb4905f45105
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-05 17:24:42 -0400

    --insert into table done

--- Weatherman/Program.cs
+++ Weatherman/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -13,7 +14,7 @@ namespace Weatherman
     {
         static void Main(string[] args)
         {
-            var localDate = DateTime.Now.ToString("HH:mm:ss tt");
+            var localDate = DateTime.Now;
             Console.WriteLine("What is your name?");
             var userName = Console.ReadLine();
             Console.WriteLine("");
@@ -21,6 +22,7 @@ namespace Weatherman
             var zipCode = Console.ReadLine();
             Console.WriteLine($"Thank you, {userName}, please enter a country code eg. us");
             var countryCode = Console.ReadLine();
+
             var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "," + countryCode + "&units=imperial&id=524901&APPID=ca27a9311ef37c705b0e639ecdfb29a6";
 
             var request = WebRequest.Create(url);
@@ -34,9 +36,38 @@ namespace Weatherman
                 
             }
 
-            var dawsonRidge = JsonConvert.DeserializeObject<RootObject>(rawResponse);
-            Console.WriteLine($"Your name: {userName} Your location: {dawsonRidge.name} and your temperature: {dawsonRidge.main.temp} deg F");
-            Console.WriteLine($"The weather conditions: {dawsonRidge.weather[0].main} at {localDate}" );
+            var weatherResult = JsonConvert.DeserializeObject<RootObject>(rawResponse);
+            Console.WriteLine($"Your name: {userName} Your location: {weatherResult.name} and your temperature: {weatherResult.main.temp} deg F");
+            Console.WriteLine($"The weather conditions: {weatherResult.weather[0].main} at {localDate}" );
+
+            const string connectionString =
+                @"Server=localhost\SQLEXPRESS;Database=Weatherman;Trusted_Connection=True;";
+
+
+
+
+
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+                var text = @"INSERT INTO Weatherman (Temperature, Conditions, Name)
+                            Values (@Temperature, @Conditions, @Name)"
+                        ;
+
+                var cmd = new SqlCommand(text, connection);
+
+                cmd.Parameters.AddWithValue("@Temperature", weatherResult.main.temp);
+                cmd.Parameters.AddWithValue("@Conditions", weatherResult.weather[0].main);
+                cmd.Parameters.AddWithValue("@Name", userName);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
         }
     }
+
 }
+
+
+
---
+++ Weatherman/Results.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weatherman
+{
+    class Results
+    {
+        public string Name { get; set; }
+        public double Temperature { get; set; }
+        public string Conditions { get; set; }
+
+
+    }
+}
