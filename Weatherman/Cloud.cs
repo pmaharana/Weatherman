e674b47964be229ee831c1a9802a28772671e4db
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-05 14:30:01 -0400

    --added classes

---
+++ Weatherman/Cloud.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weatherman
+{
+    class Cloud
+    {
+        public int all { get; set; }
+    }
+}
--- Weatherman/Program.cs
+++ Weatherman/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,27 @@ namespace Weatherman
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("What is your name?");
+            //var userName = Console.ReadLine();
+            //Console.WriteLine("");
+            //Console.WriteLine($"Welcome {userName}, please enter a zipcode and I will give you the temp");
+            //var zipCode = Console.ReadLine();
+
+            var url = "http://api.openweathermap.org/data/2.5/weather?zip=33647,us&id=524901&APPID=ca27a9311ef37c705b0e639ecdfb29a6";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+
+            var rawResponse = String.Empty;
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                rawResponse = reader.ReadToEnd();
+                Console.WriteLine(rawResponse);
+            }
+
+
+
         }
     }
 }
