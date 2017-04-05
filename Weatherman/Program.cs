9e490a888af230f7e4229b2dabf105c38e669e42
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-05 15:41:12 -0400

    --added classes and properties

--- Weatherman/Program.cs
+++ Weatherman/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,13 +13,15 @@ namespace Weatherman
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("What is your name?");
-            //var userName = Console.ReadLine();
-            //Console.WriteLine("");
-            //Console.WriteLine($"Welcome {userName}, please enter a zipcode and I will give you the temp");
-            //var zipCode = Console.ReadLine();
-
-            var url = "http://api.openweathermap.org/data/2.5/weather?zip=33647,us&id=524901&APPID=ca27a9311ef37c705b0e639ecdfb29a6";
+            var localDate = DateTime.Now.ToString("HH:mm:ss tt");
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine($"Welcome {userName}, please enter a zipcode");
+            var zipCode = Console.ReadLine();
+            Console.WriteLine($"Thank you, {userName}, please enter a country code eg. us");
+            var countryCode = Console.ReadLine();
+            var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "," + countryCode + "&units=imperial&id=524901&APPID=ca27a9311ef37c705b0e639ecdfb29a6";
 
             var request = WebRequest.Create(url);
 
@@ -28,11 +31,12 @@ namespace Weatherman
             using (var reader = new StreamReader(response.GetResponseStream()))
             {
                 rawResponse = reader.ReadToEnd();
-                Console.WriteLine(rawResponse);
+                
             }
 
-
-
+            var dawsonRidge = JsonConvert.DeserializeObject<RootObject>(rawResponse);
+            Console.WriteLine($"Your name: {userName} Your location: {dawsonRidge.name} and your temperature: {dawsonRidge.main.temp} deg F");
+            Console.WriteLine($"The weather conditions: {dawsonRidge.weather[0].main} at {localDate}" );
         }
     }
 }
